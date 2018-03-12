157e986380e82e8d7b3ae8d56272cd73741c7e93
GriffinFin <griffinfinlol@gmail.com>
2018-03-12 20:15:37 +0200

    Problem 5

---
+++ Intro-and- basic-syntax/Exercise/Project05_CharacterStats/CharacterStats.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Project05_CharacterStats
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int healthCurrent = int.Parse(Console.ReadLine());
+            int healthMax = int.Parse(Console.ReadLine());
+            int energyCurrent = int.Parse(Console.ReadLine());
+            int energyMax = int.Parse(Console.ReadLine());
+            
+            string health = new string('|', healthCurrent);
+            string healthMissing = new string('.', (healthMax - healthCurrent));
+            string energy = new string('|', energyCurrent);
+            string energyMissing = new string('.', (energyMax - energyCurrent));
+            
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Health: |{health}{healthMissing}|");
+            Console.WriteLine($"Energy: |{energy}{energyMissing}|");
+        }
+    }
+}
