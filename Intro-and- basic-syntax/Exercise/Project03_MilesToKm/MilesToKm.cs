515d496bc779eaf58552114f48250575978fdc5c
GriffinFin <griffinfinlol@gmail.com>
2018-03-12 20:10:19 +0200

    Problem 3

---
+++ Intro-and- basic-syntax/Exercise/Project03_MilesToKm/MilesToKm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Project03_MilesToKm
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            double miles = Double.Parse(Console.ReadLine());
+            double km = miles * 1.60934;
+            Console.WriteLine($"{km:f2}");
+        }
+    }
+}
