82bb3bd32013d794e4b95108d2fc2cf4dbe0e95c
GriffinFin <griffinfinlol@gmail.com>
2018-03-12 20:14:53 +0200

    Problem 4

---
+++ Intro-and- basic-syntax/Exercise/Project04_BeverageLabels/BeverageLabels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Project04_BeverageLabels
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            var volume = double.Parse(Console.ReadLine());
+            var energyContent = double.Parse(Console.ReadLine());
+            var sugarContent = double.Parse(Console.ReadLine());
+            
+            energyContent *= (volume / 100);
+            sugarContent *= (volume / 100);
+            
+            Console.WriteLine($"{volume}ml {name}:");
+            Console.WriteLine($"{energyContent}kcal, {sugarContent}g sugars");
+        }
+    }
+}
