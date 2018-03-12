a84b3b3d743da0b862f5d429b6a7503e411fe8a2
YaniLozanov <kronos1997@abv.bg>
2018-03-12 14:42:04 +0200

    First Problem is ready.

---
+++ Intro-and- basic-syntax/Exercise/Problem_01/Problem_01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Problem_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstDigit = int.Parse(Console.ReadLine());
+            int secondDigit = int.Parse(Console.ReadLine());
+            int thirdDigit = int.Parse(Console.ReadLine());
+            int fourthDigit = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{firstDigit:D4} {secondDigit:D4} {thirdDigit:D4} {fourthDigit:D4}");
+
+        }
+    }
+}
