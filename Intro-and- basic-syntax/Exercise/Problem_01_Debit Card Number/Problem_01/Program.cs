f4e233fc5dda1ed6f968d02d883d63a6d7abd853
GriffinFin <griffinfinlol@gmail.com>
2018-03-12 21:18:23 +0200

    Documented Problem 1

--- Intro-and- basic-syntax/Exercise/Problem_01_Debit Card Number/Problem_01/Program.cs
+++ Intro-and- basic-syntax/Exercise/Problem_01_Debit Card Number/Problem_01/Program.cs
@@ -6,11 +6,12 @@ namespace Problem_01
     {
         static void Main(string[] args)
         {
+            //We recieve from the Console four numbers(integers)
             int firstDigit = int.Parse(Console.ReadLine());
             int secondDigit = int.Parse(Console.ReadLine());
             int thirdDigit = int.Parse(Console.ReadLine());
             int fourthDigit = int.Parse(Console.ReadLine());
-
+            //We print the numbers to the Console in the debit card format(with leading zeroes)
             Console.WriteLine($"{firstDigit:D4} {secondDigit:D4} {thirdDigit:D4} {fourthDigit:D4}");
 
         }
