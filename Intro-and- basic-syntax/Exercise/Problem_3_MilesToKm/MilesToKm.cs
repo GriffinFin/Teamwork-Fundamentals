f3a248a62312bbb9ff065cf895b8af0e327a0d92
cleopa8 <cleopa89@gmail.com>
2018-03-13 18:36:50 +0200

    added third problem to solution

---
+++ Intro-and- basic-syntax/Exercise/Problem_3_MilesToKm/MilesToKm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Project3_MilesToKm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double mile = double.Parse(Console.ReadLine());
+            double res = mile * 1.60934;
+            Console.WriteLine("{0:f2}", res);
+        }
+    }
+}
