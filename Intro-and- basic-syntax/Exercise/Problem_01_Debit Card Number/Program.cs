a8d373efb0604b92d1b8e0830f8f86b87d4c8536
cleopa8 <cleopa89@gmail.com>
2018-03-13 19:01:54 +0200

    Change a variable name in Problem 1

--- Intro-and- basic-syntax/Exercise/Problem_01_Debit Card Number/Program.cs
+++ Intro-and- basic-syntax/Exercise/Problem_01_Debit Card Number/Program.cs
@@ -8,11 +8,11 @@ namespace Problem_01
         {
             //We recieve from the Console four numbers(integers)
             int firstDigit = int.Parse(Console.ReadLine());
-            int secondDigit = int.Parse(Console.ReadLine());
+            int digitafterfirst = int.Parse(Console.ReadLine());
             int thirdDigit = int.Parse(Console.ReadLine());
             int fourthDigit = int.Parse(Console.ReadLine());
             //We print the numbers to the Console in the debit card format(with leading zeroes)
-            Console.WriteLine($"{firstDigit:D4} {secondDigit:D4} {thirdDigit:D4} {fourthDigit:D4}");
+            Console.WriteLine($"{firstDigit:D4} {digitafterfirst:D4} {thirdDigit:D4} {fourthDigit:D4}");
 
         }
     }
